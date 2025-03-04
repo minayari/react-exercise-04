d5c3cf86e0db95b579db9a81b4ff52b8d2440ad7
MinaAllahyari <minaallahyari.uni@gmail.com>
2025-03-04 19:49:47 +0330

    feat/single product

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -1,6 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 export default function AllProducts({ image, title, price }) {
+  const navigate = useNavigate();
+
+  function handleSingelProductNav() {
+    navigate("/product", { state: { image, title, price } });
+  }
+
   return (
-    <div className="flex flex-col h-[30rem] w-[18rem] p-[1rem] mb-[1.3rem] ring-2 ring-cyan-900/60 rounded-[1rem]">
+    <div
+      onClick={handleSingelProductNav}
+      className="flex flex-col h-[30rem] w-[18rem] p-[1rem] mb-[1.3rem] ring-2 ring-cyan-900/60 rounded-[1rem]"
+    >
       <img className="h-[65%]" src={image} />
       <div className="h-[30%]">
         <h2 className="text-[1.3rem] font-bold truncate mt-[2rem] mb-[1rem]">
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,7 +1,32 @@
-export default function SingleProduct(){
-    return(
-        <>
-            <h2>hi</h2>
-        </>
-    )
+import { useLocation } from "react-router-dom";
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AlarmIcon from "@mui/icons-material/Alarm";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+
+export default function SingleProduct() {
+  const location = useLocation();
+  const { image, title, price } = location.state;
+  return (
+    <>
+      <div className="w-[20rem] h-[35rem] ring-2 ring-cyan-900/70 mx-auto my-[1rem] p-[1rem] flex flex-col rounded-[1rem]">
+        <div className=" h-[60%]">
+          <img className="w-full h-full" src={image} />
+        </div>
+        <div className="h-[20%] mt-[2rem]">
+          <h1 className="text-[1rem] font-bold text-cyan-950 my-[1rem]">
+            {title}
+          </h1>
+          <span className="text-cyan-900/80 text-[1.1rem]">${price}</span>
+        </div>
+        <div className="h-[20%] flex flex-col items-center justify-end">
+          <IconButton color="primary" aria-label="add to shopping cart">
+            <AddShoppingCartIcon />
+          </IconButton>
+        </div>
+      </div>
+    </>
+  );
 }
